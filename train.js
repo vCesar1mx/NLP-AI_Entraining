bf053a01dff2e1e6fe31784a4d8a337c0c81c338
vCesar1mx <cesarvaz.e7@gmail.com>
2022-03-31 15:36:01 -0600

    Function for check duplicated greetings added

--- train.js
+++ train.js
@@ -8,21 +8,39 @@ const fs = require("fs");
 // Let's read all our intents files in the folder intents
 const files = fs.readdirSync("./intents");
 // Looping through the files and Parsing the string to object and passing it to manager instance to train and process it.
+var textfile = 'questions.txt';
+var text = fs.readFileSync(textfile).toString();
+
 for (const file of files) {
     let data = fs.readFileSync(`./intents/${file}`);
     data = JSON.parse(data);
     const intent = file.replace(".json", "");
     for (const question of data.questions) {
+        fs.writeFileSync('questions.txt', data.questions.toString())
         manager.addDocument("es", question, intent);
+        function checkifexistwordintextfile(word) {
+
+            if (text.indexOf(word) === -1) {
+                //console.log("element doesn't exist");
+                //console.log(word)
+            }
+            else {
+                fs.appendFileSync('questionsDuplicated.txt',word.toString()+'\n')
+                console.log("Question duplicated in: " + word.toString());
+            }
+        }
+        
     }
     for (const answer of data.answers) {
         manager.addAnswer("es", intent, answer);
     }
+    checkifexistwordintextfile(data.questions.toString())
 }
 // let's create a function that will be responsible for Training and saving the manager instance.
-async function train_save(){
+async function train_save() {
     await manager.train();
     manager.save();
 }
 // Calling the above function
 train_save();
+
